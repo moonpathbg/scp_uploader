48c0b40857f9eb3fdc8b3f3dd5a3d9d8ef34685e
kandev <todor@kandev.com>
2022-02-04 21:23:19 +0200

    add exit 1 if scp fail

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ echo "Set key file access rights..."
 chmod 600 key.txt
 
 echo "Starting SCP process..."
-scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no -i key.txt $INPUT_SOURCE "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_TARGET"
-echo "SCP completed"
+scp -qrv -P $INPUT_PORT -o StrictHostKeyChecking=no -i key.txt $INPUT_SOURCE "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_TARGET" || exit 1
+echo "SCP operation completeted."
